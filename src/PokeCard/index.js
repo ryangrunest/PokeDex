192cea853de6d52a7703b013b6f19326cdc913f1
Ryan Grunest <ryangrunest45@gmail.com>
2019-07-08 11:11:38 -0400

    first commit

---
+++ src/PokeCard/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './PokeCard.css';
+
+class PokeCard extends Component {
+  render() {
+    return (
+      <div className="PokeCard">
+        hello PokeCard
+      </div>
+    )
+  }
+}
+
+export default PokeCard;
---
+++ src/PokeDex/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './PokeDex.css';
+
+class PokeDex extends Component  {
+  render() {
+    return (
+      <div className="PokeDex">
+        Hello PokeDex
+      </div>
+    )
+  }
+}
+
+export default PokeDex;

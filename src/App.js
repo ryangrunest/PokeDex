6e78b3594c583643c453234a794829af08855ca0
Ryan Grunest <ryangrunest45@gmail.com>
2019-07-08 14:19:04 -0400

    more styling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
     {id:39, name: 'Jigglypuff', type: 'normal', base_xp: 95},
     {id:94, name: 'Gengar', type: 'poison', base_xp: 225},
     {id:133, name: 'Eevee', type: 'normal', base_xp: 65},
-  ]
+  ];
+  
   return (
     <div className="App">
       <PokeDex pokemon={pokemon}/>
--- src/PokeCard/index.js
+++ src/PokeCard/index.js
@@ -5,11 +5,24 @@ class PokeCard extends Component {
   render() {
     const num = Math.floor(Math.random() * 7)
     const { id, name, type, base_xp } = this.props.pokemon.pokemon[num];
+
+    let color;
+    if (type === "fire") {
+      color = 'red';
+    } else if (type === 'water') {
+      color = 'blue';
+    } else if (type === 'bug' || type === 'poison') {
+      color = 'green';
+    } else if (type === 'electric') {
+      color = 'yellow';
+    } else if (type === 'flying' || type === 'normal') {
+      color = 'brown';
+    };
     
     return (
       <div className="PokeCard">
         <h2 className="PokeCard-Name">{name}</h2>
-        <h4 className="PokeCard-Type">Type: {type}</h4>
+        <h4 className="PokeCard-Type">Type: <span style={{color: color}}>{type}</span></h4>
         <p className="PokeCard-points">Experience: {base_xp}</p>
         <img alt="charmander" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} className="PokeCard-Image" />
       </div>

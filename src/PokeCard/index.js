5084e7d8391724e53406d496e426bb678e85215a
Ryan Grunest <ryangrunest45@gmail.com>
2019-07-14 13:40:10 -0700

    final styling

--- src/PokeCard/index.js
+++ src/PokeCard/index.js
@@ -21,11 +21,11 @@ class PokeCard extends Component {
     return (
       <div className="PokeCard">
         <h2 className="PokeCard-Name">{name}</h2>
-        <h4 className="PokeCard-Type">Type: <span style={{color: color}}>{type}</span></h4>
-        <p className="PokeCard-points">Experience: {exp}</p>
         <div className="PokeCard-img-holder">
           <img className="PokeCard-Image" alt="pokemon" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
         </div>
+        <h4 className="PokeCard-Type">Type: <span style={{color: color}}>{type}</span></h4>
+        <p className="PokeCard-points">Experience: {exp}</p>
       </div>
     )
   }
